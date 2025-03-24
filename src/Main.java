13a5bfd33914869892a174879f2dc58e01038cf4
Vasil Popov <vasipopov@tu-sofia.bg>
2025-03-24 22:28:33 +0200

    xd

--- src/Main.java
+++ src/Main.java
@@ -51,11 +51,15 @@ public class Main {
     public static class AuthResponse {
         private String username;
         private String role;
+        private String email;
+        private String phone;
         private String token;
 
-        public AuthResponse(String username, String role, String token) {
+        public AuthResponse(String username, String role, String email, String phone, String token) {
             this.username = username;
             this.role = role;
+            this.email = email;
+            this.phone = phone;
             this.token = token;
         }
 
@@ -65,6 +69,14 @@ public class Main {
 
         public String getRole() {
             return role;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+        
+        public String getPhone() {
+            return phone;
         }
 
         public String getToken() {
