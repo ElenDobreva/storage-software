bf93d8bab63241f726246351be720d3e8ff1f5ff
Vasil Popov <vasipopov@tu-sofia.bg>
2025-03-28 21:18:19 +0200

    refactor Product ID type from Long to Integer in ProductController and ProductRepository

--- src/ProductRepository.java
+++ src/ProductRepository.java
@@ -2,5 +2,5 @@ package com.warehouse.backend;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
