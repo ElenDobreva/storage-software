0041ec8dc92fb82bc0a1a7fba1e1a971d29dfb04
Denislav Ivanov <denislavivanov82@gmail.com>
2025-03-30 22:50:26 +0300

    Bug fix: missing package declaration in 'WebConfig.java'

--- src/WebConfig.java
+++ src/WebConfig.java
@@ -1,3 +1,5 @@
+package com.warehouse.backend;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
